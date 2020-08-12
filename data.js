ed5262750c64402452ac83b1cbf94e081a56b882
Dmitry Kolosov <mitrichius@gmail.com>
2020-08-12 21:34:07 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,204 @@
+let data = {
+    "config": {
+        "image_default": "https://s3.amazonaws.com/gt7sp-prod/decal/20/65/90/4684386837524906520_1.png",
+        "currency_default": "руб",
+    },
+    "items": [
+        {
+            "name": "Apple iPad 2019 32GB Wi-Fi Space Gray",
+            "price": 26000,
+            "tags": ["техника"],
+            "url": "https://biggeek.ru/products/apple-ipad-2019-32gb-wi-fi-space-gray",
+            "image": "https://cdn2.biggeek.ru/1/435/711c/9900-385ipad-wifi-select-space-201909_GEO_RU.png"
+        },
+        {
+            "name": "Lego Экстремальный внедорожник",
+            "price": 12500,
+            "tags": ["lego"],
+            "url": "https://mir-kubikov.ru/lego/42099/",
+            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/6b2/670_520_140cd750bba9870f18aada2478b24840a/6b23f2e4088abd8b6f802fa4e1792f92.jpg",
+        },
+        {
+            "name": "Lego Batmobile",
+            "price": 20000,
+            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/b2a/746_580_140cd750bba9870f18aada2478b24840a/b2af09393c7e0bc105372029af274a56.jpg",
+            "tags": ["lego"],
+            "url": "https://mir-kubikov.ru/lego/76139/",
+        },
+        {
+            "name": "Рюкзак Nomatic Travel Pack",
+            "price": 21600,
+            "image": "https://cdn.shopify.com/s/files/1/0745/1299/products/travel-pack_1024x1024.jpg?v=1588217895",
+            "tags": ["полезное"],
+            "url": "https://www.bagandwallet.ru/collection/nomatic/product/nomatic_travel_pack_kupit",
+        },
+        {
+            "name": "Техносвященники. Том первый",
+            "price": 1400,
+            "tags": ["комиксы"],
+            "url": "https://www.labirint.ru/books/731757/",
+            "image": "https://img3.labirint.ru/rc/9ee30ed65a26f742e7dac59db4abeba3/220x340/books74/731757/cover.jpg?1577273210"
+        },  
+        {
+            "name": "Техносвященники. Том второй",
+            "price": 1240,
+            "tags": ["комиксы"],
+            "url": "https://www.labirint.ru/books/744320/",
+            "image": "https://img4.labirint.ru/rc/c1b5ccf2fc15a72a72349dcd767a0980/220x340/books75/744320/cover.jpg?1585200359"
+        },  
+        {
+            "name": "Комикс Preacher (Hardcover) Book Four и последующие",
+            "price": 27,
+            "currency": "$",
+            "tags": ["комиксы"],
+            "url": "https://www.amazon.com/Preacher-Book-Four-Garth-Ennis/dp/1401230938",
+            "image": "https://cdn1.ozone.ru/multimedia/1010438854.jpg"
+        },
+        {
+            "name": "New Super Mario Bros U Deluxe",
+            "price": 3800,
+            "tags": ["игры", "switch"],
+            "url": "https://www.mvideo.ru/products/igra-nintendo-switch-new-super-mario-bros-u-deluxe-40070863",
+            "image": "https://img.mvideo.ru/Pdb/40070863b.jpg"
+        },
+        {
+            "name": "Трансметрополитен. Книга 1",
+            "price": 900,
+            "tags": ["комиксы"],
+            "url": "https://www.ozon.ru/context/detail/id/178094824",
+            "image": "https://cdn1.ozone.ru/s3/multimedia-y/wc1200/6018351430.jpg"
+        },
+        {
+            "name": "Футболка Hollow Knight: Troupe Master",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/collections/hollow-knight/products/grimm-hollow-knight-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_HK_troupe_master_shirt_photo1_1024x1024.png?v=1563036118"
+        },
+        {
+            "name": "Футболка Bloodborne: Blades of Mercy",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/collections/bloodborne/products/bloodborne-blades-of-mercy-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_bb_blades-of-mercy_shirt_photo1_1024x1024.png?v=1585065548"
+        },
+        {
+            "name": "Футболка God of War: Greatest Challenge",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/products/god-of-war-lake-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_GoW_greatestchallenge_shirt_photo1_1024x1024.png?v=1569533503"
+        },
+        {
+            "name": "Футболка Celeste: Sides",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/collections/celeste/products/celeste-cassette-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_celeste_sides_shirt_photo1_1024x1024.png?v=1580160195"
+        },
+        {
+            "name": "Футболка Myst: Mystic Arcana",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/collections/myst/products/myst-arcana-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_myst_mystic-arcana_shirt_main_1024x1024.png?v=1594744035"
+        },
+        {
+            "name": "Футболка Dead Cells: The Island",
+            "price": 24,
+            "currency": "$",
+            "tags": ["футболки"],
+            "url": "https://www.fangamer.com/collections/dead-cells/products/dead-cells-logo-shirt",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_DeadCells_TheIsland_shirt_photo1_1024x1024.png?v=1566606405"
+        },
+        {
+            "name": "Провиденс",
+            "price": 2400,
+            "tags": ["комиксы"],
+            "url": "https://www.labirint.ru/books/720524/",
+            "image": "https://img4.labirint.ru/rc/26b26169431ffbede6116ca6056ad601/220x340/books73/720524/cover.jpg?1570520268"
+        },
+        {
+            "name": "DOOM Slayer Tubbz Collectible Duck",
+            "price": 13,
+            "currency": "£",
+            "tags": ["утки"],
+            "url": "https://www.geekstore.com/products/doom-slayer-tubbz-collectible-duck",
+            "image": "https://cdn.shopify.com/s/files/1/1375/9903/products/tubbz-doom-doom-slayer-gs-01_800x.progressive.jpg?v=1592333056"
+        },
+        {
+            "name": "Dark Souls: Heroes of Lordran - Solaire Figurine",
+            "price": 19,
+            "currency": "$",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_DS_figs_solairemain1_1256583f-3db6-4c77-8f79-d6770a584471_1024x1024.png?v=1543344725",
+            "tags": ["фигурки"],
+            "url": "https://www.fangamer.com/collections/dark-souls/products/dark-souls-solaire-figurine",
+            "date": "2020-07-09"
+        },
+        {
+            "name": "Destiny Cayde-6 Tubbz Collectible Duck",
+            "price": 13,
+            "currency": "£",
+            "tags": ["утки"],
+            "url": "https://www.geekstore.com/products/destiny-cayde-6-tubbz-cosplaying-duck-collectible-figure",
+            "image": "https://cdn.shopify.com/s/files/1/1375/9903/products/destiny-cayde-6-gs-800x1000-01_800x.progressive.jpg?v=1592333102"
+        },
+        {
+            "name": "Icewind Dale: Enhanced Edition и Planescape Torment: Enhanced Edition",
+            "price": 3000,
+            "tags": ["игры", "switch"],
+            "url": "https://mir.nintendo.ru/shop/product/icewind-dale-enhanced-edition-i-planescape-torment-enhanced-edition-nintendo-1811949031029",
+            "image": "https://www.gamepark.ru/upload/iblock/d6b/144c3bc64b438a4260af08f11693d5e1.jpg"
+        },
+        {
+            "name": "The Legend of Zelda: Link's Awakening",
+            "price": 3800,
+            "tags": ["игры", "switch"],
+            "url": "https://www.mvideo.ru/products/igra-nintendo-switch-the-legend-of-zelda-link-s-awakening-40071643",
+            "image": "https://img.mvideo.ru/Pdb/40071643b.jpg"
+        },
+        {
+            "name": "Hollow Knight Pin",
+            "price": 10,
+            "currency": "$",
+            "tags": ["pins"],
+            "url": "https://www.fangamer.com/products/hollow-knight-pin",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_HK_wanderer_lapel_main_1024x1024.jpg?v=1541195039"
+        },
+        {
+            "name": "Saw Cleaver Pin",
+            "price": 12,
+            "currency": "$",
+            "tags": ["pins"],
+            "url": "https://www.fangamer.com/collections/bloodborne/products/bloodborne-saw-cleaver-pin",
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_bloodborne_plain-doll-hand_pin_main_e65bc9c5-8daf-4b52-9072-ee0204c7a389_1024x1024.png?v=1592247200"
+        },
+        {
+            "name": "Стикерпак Fallout",
+            "price": 250,
+            "image": "https://www.parazitakusok.ru/images/item/3705/bez_imeni-1.jpg",
+            "tags": ["стикеры"],
+            "url": "https://www.parazitakusok.ru/item/3705/",
+            "date": "2020-07-08"
+        },
+        {
+            "name": "1922 BY J.M. Keune Essential Conditioner",
+            "price": 1750,
+            "tags": ["борода", "уход за собой"],
+            "url": "http://keune.store/product/keune-1922-universalnyy-konditsioner-dlya-volos-i-borody",
+            "image": "https://static-ru.insales.ru/images/products/1/7616/171867584/large_Keune_1922_%D0%A3%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BD%D0%B4%D0%B8%D1%86%D0%B8%D0%BE%D0%BD%D0%B5%D1%80_%D0%B4%D0%BB%D1%8F_%D0%B2%D0%BE%D0%BB%D0%BE%D1%81_%D0%B8_%D0%B1%D0%BE%D1%80%D0%BE%D0%B4%D1%8B_250.jpg"
+        },
+        {
+            "name": "1922 BY J.M. Keune Styling Premier Paste",
+            "price": 1700,
+            "tags": ["уход за собой"],
+            "url": "https://hairs-russia.ru/brendy/keune-niderlandy/1922-styling-stajling/keune-1922-styling-premier-paste---premer-pasta-75-ml",
+            "image": "https://hairs-russia.ru/image/cache/350-438/data/Keune/keune-1922-styling-premier-paste-75-ml.jpeg"
+        },
+    ]
+}
