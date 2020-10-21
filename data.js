8bf6fadce393bc4c2ad432656337e7b25ea0c992
Dmitry Kolosov <mitrichius@gmail.com>
2020-10-21 19:00:48 +0300

    updates

--- data.js
+++ data.js
@@ -2,35 +2,18 @@ let data = {
     "config": {
         "image_default": "https://s3.amazonaws.com/gt7sp-prod/decal/20/65/90/4684386837524906520_1.png",
         "currency_default": "руб",
+        //"description": "Для бронирования обращаться к <a href=\"tg://resolve?domain=FireKriS\">Крис</a>",
+        "show_archived": 1,
     },
     "items": [
         {
-            "name": "Apple iPad 2019 32GB Wi-Fi Space Gray",
-            "price": 26000,
-            "tags": ["техника"],
-            "url": "https://biggeek.ru/products/apple-ipad-2019-32gb-wi-fi-space-gray",
-            "image": "https://cdn2.biggeek.ru/1/435/711c/9900-385ipad-wifi-select-space-201909_GEO_RU.png"
-        },
-        {
-            "name": "Lego Экстремальный внедорожник",
-            "price": 12500,
-            "tags": ["lego"],
-            "url": "https://mir-kubikov.ru/lego/42099/",
-            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/6b2/670_520_140cd750bba9870f18aada2478b24840a/6b23f2e4088abd8b6f802fa4e1792f92.jpg",
-        },
-        {
-            "name": "Lego Batmobile",
-            "price": 20000,
-            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/b2a/746_580_140cd750bba9870f18aada2478b24840a/b2af09393c7e0bc105372029af274a56.jpg",
-            "tags": ["lego"],
-            "url": "https://mir-kubikov.ru/lego/76139/",
-        },
-        {
-            "name": "Рюкзак Nomatic Travel Pack",
-            "price": 21600,
-            "image": "https://cdn.shopify.com/s/files/1/0745/1299/products/travel-pack_1024x1024.jpg?v=1588217895",
-            "tags": ["полезное"],
-            "url": "https://www.bagandwallet.ru/collection/nomatic/product/nomatic_travel_pack_kupit",
+            "name": "Сертификат от Валеры",
+            "price": "5к to infinity and beyond",
+            "currency": "(руб)",
+            "image": "tattoo-cert.jpg",
+            "tags": ["тату"],
+            "url": "https://www.instagram.com/valera_fedya/",
+            "multi": 1,
         },
         {
             "name": "Техносвященники. Том первый",
@@ -38,14 +21,29 @@ let data = {
             "tags": ["комиксы"],
             "url": "https://www.labirint.ru/books/731757/",
             "image": "https://img3.labirint.ru/rc/9ee30ed65a26f742e7dac59db4abeba3/220x340/books74/731757/cover.jpg?1577273210"
-        },  
+        },
         {
             "name": "Техносвященники. Том второй",
             "price": 1240,
             "tags": ["комиксы"],
             "url": "https://www.labirint.ru/books/744320/",
-            "image": "https://img4.labirint.ru/rc/c1b5ccf2fc15a72a72349dcd767a0980/220x340/books75/744320/cover.jpg?1585200359"
-        },  
+            "image": "https://img4.labirint.ru/rc/c1b5ccf2fc15a72a72349dcd767a0980/220x340/books75/744320/cover.jpg?1585200359",
+        },
+        {
+            "name": "Hacker gift",
+            "price": 20,
+            "currency": "$",
+            "tags": ["geek"],
+            "url": "https://hacker.gifts/",
+            "image": "https://cdn.shopify.com/s/files/1/0013/8539/8385/products/happy-programmers-day_960x.png?v=1537068351",
+        },
+        {
+            "name": "Гарнитура PlayStation Platinum",
+            "price": 9100,
+            "tags": ["гейминг"],
+            "url": "https://www.ozon.ru/context/detail/id/138118554/",
+            "image": "https://cdn1.ozone.ru/multimedia/wc1200/1015727002.jpg"
+        },
         {
             "name": "Комикс Preacher (Hardcover) Book Four и последующие",
             "price": 27,
@@ -73,7 +71,8 @@ let data = {
             "price": 4000,
             "tags": ["настолки"],
             "url": "https://hobbygames.ru/pokorenie-marsa",
-            "image": "https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Lavka_Games/Pokorenie_Marsa/Proekt_Venera/Pokorenie_Marsa-1024x1024-wm.jpg"
+            "image": "https://hobbygames.cdnvideo.ru/image/cache/hobbygames_beta/data/Lavka_Games/Pokorenie_Marsa/Proekt_Venera/Pokorenie_Marsa-1024x1024-wm.jpg",
+            "archived": 1,
         },
         {
             "name": "Футболка Hollow Knight: Troupe Master",
@@ -81,7 +80,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/collections/hollow-knight/products/grimm-hollow-knight-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_HK_troupe_master_shirt_photo1_1024x1024.png?v=1563036118"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_HK_troupe_master_shirt_photo1_1024x1024.png?v=1563036118",
+            "description": "Размер M",
         },
         {
             "name": "Футболка Bloodborne: Blades of Mercy",
@@ -89,7 +89,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/collections/bloodborne/products/bloodborne-blades-of-mercy-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_bb_blades-of-mercy_shirt_photo1_1024x1024.png?v=1585065548"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_bb_blades-of-mercy_shirt_photo1_1024x1024.png?v=1585065548",
+            "description": "Размер M",
         },
         {
             "name": "Футболка God of War: Greatest Challenge",
@@ -97,7 +98,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/products/god-of-war-lake-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_GoW_greatestchallenge_shirt_photo1_1024x1024.png?v=1569533503"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_GoW_greatestchallenge_shirt_photo1_1024x1024.png?v=1569533503",
+            "description": "Размер M",
         },
         {
             "name": "Футболка Celeste: Sides",
@@ -105,7 +107,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/collections/celeste/products/celeste-cassette-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_celeste_sides_shirt_photo1_1024x1024.png?v=1580160195"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_celeste_sides_shirt_photo1_1024x1024.png?v=1580160195",
+            "description": "Размер M",
         },
         {
             "name": "Футболка Myst: Mystic Arcana",
@@ -113,7 +116,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/collections/myst/products/myst-arcana-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_myst_mystic-arcana_shirt_main_1024x1024.png?v=1594744035"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_myst_mystic-arcana_shirt_main_1024x1024.png?v=1594744035",
+            "description": "Размер M",
         },
         {
             "name": "Футболка Dead Cells: The Island",
@@ -121,7 +125,8 @@ let data = {
             "currency": "$",
             "tags": ["футболки"],
             "url": "https://www.fangamer.com/collections/dead-cells/products/dead-cells-logo-shirt",
-            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_DeadCells_TheIsland_shirt_photo1_1024x1024.png?v=1566606405"
+            "image": "https://cdn.shopify.com/s/files/1/0014/1962/products/product_DeadCells_TheIsland_shirt_photo1_1024x1024.png?v=1566606405",
+            "description": "Размер M",
         },
         {
             "name": "Провиденс",
@@ -156,19 +161,20 @@ let data = {
             "image": "https://cdn.shopify.com/s/files/1/1375/9903/products/destiny-cayde-6-gs-800x1000-01_800x.progressive.jpg?v=1592333102"
         },
         {
-            "name": "Icewind Dale: Enhanced Edition и Planescape Torment: Enhanced Edition",
-            "price": 3000,
-            "tags": ["игры", "switch"],
-            "url": "https://mir.nintendo.ru/shop/product/icewind-dale-enhanced-edition-i-planescape-torment-enhanced-edition-nintendo-1811949031029",
-            "image": "https://www.gamepark.ru/upload/iblock/d6b/144c3bc64b438a4260af08f11693d5e1.jpg"
-        },
-        {
             "name": "The Legend of Zelda: Link's Awakening",
             "price": 3800,
             "tags": ["игры", "switch"],
             "url": "https://www.mvideo.ru/products/igra-nintendo-switch-the-legend-of-zelda-link-s-awakening-40071643",
             "image": "https://img.mvideo.ru/Pdb/40071643b.jpg"
         },
+        {
+            "name": "Виски Oban malt 14 years old",
+            "price": 6500,
+            "tags": ["алкоголь"],
+            "url": "https://winestyle.ru/products/Oban-malt-14-years-old-with-box.html",
+            "image": "https://skygarden.azureedge.net/media/images/products/2019/02/product_image_66b99a62-658d-4018-a8ea-998a174b0231.jpg"
+        },
+
         {
             "name": "Hollow Knight Pin",
             "price": 10,
@@ -198,14 +204,48 @@ let data = {
             "price": 1750,
             "tags": ["борода", "уход за собой"],
             "url": "http://keune.store/product/keune-1922-universalnyy-konditsioner-dlya-volos-i-borody",
-            "image": "https://static-ru.insales.ru/images/products/1/7616/171867584/large_Keune_1922_%D0%A3%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BD%D0%B4%D0%B8%D1%86%D0%B8%D0%BE%D0%BD%D0%B5%D1%80_%D0%B4%D0%BB%D1%8F_%D0%B2%D0%BE%D0%BB%D0%BE%D1%81_%D0%B8_%D0%B1%D0%BE%D1%80%D0%BE%D0%B4%D1%8B_250.jpg"
+            "image": "https://static-ru.insales.ru/images/products/1/7616/171867584/large_Keune_1922_%D0%A3%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BD%D0%B4%D0%B8%D1%86%D0%B8%D0%BE%D0%BD%D0%B5%D1%80_%D0%B4%D0%BB%D1%8F_%D0%B2%D0%BE%D0%BB%D0%BE%D1%81_%D0%B8_%D0%B1%D0%BE%D1%80%D0%BE%D0%B4%D1%8B_250.jpg",
+            "multi": 1,
         },
         {
             "name": "1922 BY J.M. Keune Styling Premier Paste",
             "price": 1700,
             "tags": ["уход за собой"],
             "url": "https://hairs-russia.ru/brendy/keune-niderlandy/1922-styling-stajling/keune-1922-styling-premier-paste---premer-pasta-75-ml",
-            "image": "https://hairs-russia.ru/image/cache/350-438/data/Keune/keune-1922-styling-premier-paste-75-ml.jpeg"
+            "image": "https://hairs-russia.ru/image/cache/350-438/data/Keune/keune-1922-styling-premier-paste-75-ml.jpeg",
+            "multi": 1,
+        },
+        {
+            "name": "Apple iPad 2019 32GB Wi-Fi Space Gray",
+            "price": 26000,
+            "tags": ["техника"],
+            "url": "https://biggeek.ru/products/apple-ipad-2019-32gb-wi-fi-space-gray",
+            "image": "https://cdn2.biggeek.ru/1/435/711c/9900-385ipad-wifi-select-space-201909_GEO_RU.png",
+            "archived": 1,
+        },
+        {
+            "name": "Lego Экстремальный внедорожник",
+            "price": 12500,
+            "tags": ["lego"],
+            "url": "https://mir-kubikov.ru/lego/42099/",
+            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/6b2/670_520_140cd750bba9870f18aada2478b24840a/6b23f2e4088abd8b6f802fa4e1792f92.jpg",
+            "archived": 1,
+        },
+        {
+            "name": "Lego Batmobile",
+            "price": 20000,
+            "image": "https://mir-kubikov.ru/upload/resize_cache/iblock/b2a/746_580_140cd750bba9870f18aada2478b24840a/b2af09393c7e0bc105372029af274a56.jpg",
+            "tags": ["lego"],
+            "url": "https://mir-kubikov.ru/lego/76139/",
+            "archived": 1,
+        },
+        {
+            "name": "Рюкзак Nomatic Travel Pack",
+            "price": 21600,
+            "image": "https://cdn.shopify.com/s/files/1/0745/1299/products/travel-pack_1024x1024.jpg?v=1588217895",
+            "tags": ["полезное"],
+            "url": "https://www.bagandwallet.ru/collection/nomatic/product/nomatic_travel_pack_kupit",
+            "archived": 1,
         },
     ]
 }
