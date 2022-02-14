7f8fb53222a3a2f3133274b2b2032c4662f8b9e4
Musatttt <98084118+Musatttt@users.noreply.github.com>
2022-02-14 18:12:11 +0300

    Update data.js

--- data.js
+++ data.js
@@ -32,12 +32,12 @@ let data = {
             "image": "https://cdn.vseinstrumenti.ru/images/goods/rashodnie-materialy/instrument/765125/560x504/51447978.jpg",
         },
          {
-            "name": "Трёхосевой стедикам Moza Mini-MX MSG02",
-            "price": 6999,
+            "name": "Трёхосевой стедикам DJI Osmo Mobile 4 SE",
+            "price": 9990,
             "currency": "₽",
             "tags": ["техника"],
-            "url": "https://www.svyaznoy.ru/catalog/accessories/10613/6051291?utm_medium=cpc&utm_source=google&utm_campaign=cid|1708225833|cn|mg_g_ot_sh_nc_npc_nv_nm_nt_a_s3_rf_n_rt&utm_term=ph|pla-296275983289|kw|&utm_content=mgc|%D1%81|331726892323|ph|pla-296275983289|kw||aid|68160304993|n|u|p||t||at||mid|10313383|pc|online|pid|6051291_135|pco|RU|pl|ru|ppi|296275983289|sc||fid||mt||d|c|apid||lpm|9047029|lim||dm|&gclid=Cj0KCQiAmKiQBhClARIsAKtSj-k6ATbat-h3Z6ygC0a4eZ11JKyqzBTpFYe4gzsjZVRrR1fdmvjl88UaAvQYEALw_wcB",
-            "image": "https://cdn.svyaznoy.ru/upload/iblock/5a1/elektronnyy_trekhosevoy_stedikam_moza_mini_mx_1575646_1.jpg/resize/483x483/hq/",
+            "url": "https://www.svyaznoy.ru/catalog/accessories/10613/5818145",
+            "image": "https://cdn.svyaznoy.ru/upload/iblock/927/osmo%20mobile%204.jpg/resize/870x725/hq/",
         },
         {
             "name": "HONOR MagicWatch 2 46 мм",
